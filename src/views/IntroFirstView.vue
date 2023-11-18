b81455e855d87d638b162590daf76bd546d6b9b4
mafiapatch <g8071107@gmail.com>
2023-11-18 20:02:59 +0800

    [modify] progress backup

--- src/views/IntroFirstView.vue
+++ src/views/IntroFirstView.vue
@@ -3,12 +3,11 @@
 
 <template>
   <main class="yellow_bg">
-    <div class="left_top_logo"></div>
+    <div class="left_top_logo_full"></div>
     <div class="right_top_play_button"></div>
-    <div class="main_product_full"></div>
+    <div class="left_down_woman_full"></div>
     <div class="left_down_cloud"></div>
-    <div class="right_down_belt"></div>
-    <div class="dragon_right_small"></div>
-    <div class="ground"></div>
+    <div class="dragon_right_big_full"></div>
+    <div class="ground_wave"></div>
   </main>
 </template>
