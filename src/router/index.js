91d18f3d649bc3f5176df6f0bf7c2f6c8e72f888
mafiapatch <g8071107@gmail.com>
2023-11-18 17:00:47 +0800

    [modify] intro 1 view

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import IntroFirstView from "../views/IntroFirstView.vue";
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,11 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+    },
+    {
+      path: "/intro/1",
+      name: "intro_1",
+      component: IntroFirstView,
     },
   ],
 });
---
+++ src/views/IntroFirstView.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+  <main class="yellow_bg">
+    <div class="left_top_logo"></div>
+    <div class="right_top_play_button"></div>
+    <div class="main_product_full"></div>
+    <div class="left_down_cloud"></div>
+    <div class="right_down_belt"></div>
+    <div class="dragon_right_small"></div>
+    <div class="ground"></div>
+  </main>
+</template>
