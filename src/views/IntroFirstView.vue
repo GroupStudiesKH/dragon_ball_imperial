08a9f5fbf15db0d91cb01605212aa89f11e61781
mafiapatch <g8071107@gmail.com>
2023-11-18 20:10:15 +0800

    [modify] add text

--- src/views/IntroFirstView.vue
+++ src/views/IntroFirstView.vue
@@ -3,6 +3,12 @@
 
 <template>
   <main class="yellow_bg">
+    <div class="intro_slogan">
+        完成收集好運8龍珠，<br>
+        就能獲得好運御守，<br>
+        讓您好運不斷<br>
+        龍年行大運！
+    </div>
     <div class="left_top_logo_full"></div>
     <div class="right_top_play_button"></div>
     <div class="left_down_woman_full"></div>
