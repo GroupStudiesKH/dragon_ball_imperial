a9ccf4aca9915caae436f46ec095e43e8495be64
mafiapatch <g8071107@gmail.com>
2023-11-18 16:39:22 +0800

    [modify] index finish

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,9 @@
 
 <template>
   <main class="yellow_bg">
+    <div class="left_top_logo"></div>
+    <div class="right_top_play_button"></div>
+    <div class="index_title_full"></div>
     <div class="main_product_full"></div>
     <div class="left_down_cloud"></div>
     <div class="right_down_belt"></div>
