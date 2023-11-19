63dc1c0bb09f4bdf0e6522ead7479ec2de07b6b6
mafiapatch <g8071107@gmail.com>
2023-11-20 06:44:48 +0800

    [modify] Update Vite config and add custom Service Worker

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,8 @@ export default defineConfig({
       includeAssets: ['/**/*.js', '/**/*.png', '/**/*.jpg', '/**/*.jpeg', '/**/*.gif', '/**/*.svg'],
       manifest: {
         display: 'fullscreen',
-      }
+      },
+      swSrc: 'src/custom-service-worker.js', // Specify the path to your custom Service Worker file
     })
   ],
   resolve: {
