1f41b87ac63b1680c5323254bc1e36ed0dfcf5cf
mafiapatch <g8071107@gmail.com>
2023-11-20 06:52:31 +0800

    [modify] update sort

--- src/main.js
+++ src/main.js
@@ -13,21 +13,23 @@ app.mount("#app");
 
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js').then((registration) => {
-      // 在這裡可以使用 registration 來進一步管理 Service Worker
-      console.log('Service Worker 注冊成功:', registration);
+        // 在這裡可以使用 registration 來進一步管理 Service Worker
+        console.log('Service Worker 注冊成功:', registration);
+
+        navigator.serviceWorker.addEventListener('message', (event) => {
+            if (event.data && event.data.type === 'syncProgress') {
+                console.log('進度:', event.data.progress);
+                // 在這裡更新網頁上的進度顯示
+            }
+        });
+
+        // 觸發 Service Worker 檢查 Cache Storage 狀態
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.controller.postMessage({
+                type: 'checkCacheStorage',
+            });
+        }
     });
-  }
-  
-  navigator.serviceWorker.addEventListener('message', (event) => {
-    if (event.data && event.data.type === 'syncProgress') {
-      console.log('進度:', event.data.progress);
-      // 在這裡更新網頁上的進度顯示
-    }
-  });
-  
-  // 觸發 Service Worker 檢查 Cache Storage 狀態
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.controller.postMessage({
-      type: 'checkCacheStorage',
-    });
-  }
+
+}
+
