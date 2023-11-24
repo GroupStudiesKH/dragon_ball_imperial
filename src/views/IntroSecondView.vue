116e26aa1e85aeee696c2fc2a55f1a3c772ce3cd
mafiapatch <g8071107@gmail.com>
2023-11-25 00:48:02 +0800

    [modify] link

--- src/views/IntroSecondView.vue
+++ src/views/IntroSecondView.vue
@@ -1,11 +1,26 @@
-<script setup>
+<script>
+import { useRouter, useRoute } from "vue-router";
+
+export default {
+
+  setup() {
+    const router = useRouter();
+
+    const toNextPage = () => {
+      router.push({ name: "game" });
+    };
+    return {
+      toNextPage
+    };
+  },
+};
 </script>
 
 <template>
   <main class="yellow_bg">
     <div class="intro_2_main_full"></div>
     <div class="intro_2_main_title"></div>
-    <div class="right_top_play_button"></div>
+    <div class="right_top_play_button" @click="toNextPage()"></div>
     <div class="left_down_woman_small"></div>
     <div class="left_down_cloud"></div>
     <div class="ground_wave"></div>
