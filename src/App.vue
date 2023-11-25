4d1918050c1c71b6e5a1951113970333db91979d
mafiapatch <g8071107@gmail.com>
2023-11-25 22:56:04 +0800

    [modify] test

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,32 @@
-<script setup>
+<script>
 import { RouterLink, RouterView } from "vue-router";
+import { onMounted, reactive } from "vue";
+export default {
+
+  setup() {
+    const states = reactive({
+      deferredPrompt: null,
+    });
+    onMounted(() => {
+      window.addEventListener("beforeinstallprompt", e => {
+        e.preventDefault();
+        states.deferredPrompt = e;
+      });
+      window.addEventListener("appinstalled", () => {
+        states.deferredPrompt = null;
+        alert("app installed");
+      });
+      document.querySelector("#app").addEventListener("click", () => { 
+        if (states.deferredPrompt) {
+          states.deferredPrompt.prompt();
+          states.deferredPrompt = null;
+        }
+      });
+    });
+
+  },
+};
+
 </script>
 
 <template>
