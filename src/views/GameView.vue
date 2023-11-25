ab1e0bd880bf4b17b4bdfbc602694ed2ddf8870b
mafiapatch <g8071107@gmail.com>
2023-11-25 12:53:54 +0800

    [modify] css ball

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -3,28 +3,40 @@ import { ref, onMounted } from "vue";
 import { useRouter, useRoute } from "vue-router";
 
 export default {
-
   setup() {
     const router = useRouter();
-    const countdown = ref(4)
+    const countdown = ref(4);
+    const balls = ref([[], [], [], [], []]);
+
+    const createBalls = () => {
+      const ballCount = balls.value.length;
+      const rail = Math.floor(Math.random() * ballCount);
+      balls.value[rail].push({
+        top: 0
+      });
+    };
 
     //countdown onmounted
     onMounted(() => {
       const timer = setInterval(() => {
-        countdown.value--
-        console.log(countdown.value)
+        countdown.value--;
         if (countdown.value === 0) {
-          clearInterval(timer)
+          clearInterval(timer);
+
+            // setInterval(() => {
+            //     createBalls();
+            // }, 1000);
         }
-      }, 1000)
-    })
+      }, 1000);
+    });
 
     const toNextPage = () => {
       router.push({ name: "intro_1" });
     };
     return {
       toNextPage,
-      countdown
+      countdown,
+      balls,
     };
   },
 };
@@ -34,6 +46,43 @@ export default {
   <main class="game_bg">
     <div class="game_header"></div>
     <div class="game_frame"></div>
+    <div class="game_ball_rails">
+      <div class="game_ball_rail_1">
+        <div
+          class="game_ball"
+          v-for="(ball, ballIndex) in balls[0]"
+          v-bind:key="ballIndex"
+        ></div>
+      </div>
+      <div class="game_ball_rail_2">
+        <div
+          class="game_ball"
+          v-for="(ball, ballIndex) in balls[1]"
+          v-bind:key="ballIndex"
+        ></div>
+      </div>
+      <div class="game_ball_rail_3">
+        <div
+          class="game_ball"
+          v-for="(ball, ballIndex) in balls[2]"
+          v-bind:key="ballIndex"
+        ></div>
+      </div>
+      <div class="game_ball_rail_4">
+        <div
+          class="game_ball"
+          v-for="(ball, ballIndex) in balls[3]"
+          v-bind:key="ballIndex"
+        ></div>
+      </div>
+      <div class="game_ball_rail_5">
+        <div
+          class="game_ball"
+          v-for="(ball, ballIndex) in balls[4]"
+          v-bind:key="ballIndex"
+        ></div>
+      </div>
+    </div>
     <div class="game_dragon"></div>
     <div class="countdown countdown_3" v-if="countdown == 3"></div>
     <div class="countdown countdown_2" v-if="countdown == 2"></div>
