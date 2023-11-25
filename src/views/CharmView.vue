050358f71cbd4a847baf69689afa3d20e044e04d
mafiapatch <g8071107@gmail.com>
2023-11-25 22:07:22 +0800

    [modify] charm 3 ball

--- src/views/CharmView.vue
+++ src/views/CharmView.vue
@@ -7,7 +7,7 @@ export default {
     const router = useRouter();
 
     const toNextPage = () => {
-      router.push({ name: "intro_1" });
+      router.push({ name: "final" });
     };
     return {
       toNextPage,
@@ -20,6 +20,9 @@ export default {
   <main class="charm_bg">
     <div class="right_top_play_button" @click="toNextPage()"></div>
     <div class="charm_main"></div>
+    <div class="charm_ball charm_ball_cloud"></div>
+    <div class="charm_ball charm_ball_wind"></div>
+    <div class="charm_ball charm_ball_infinite"></div>
     <div class="dragon_right_big_full"></div>
     <div class="right_down_belt_without_slogan"></div>
     <div class="ground_wave"></div>
