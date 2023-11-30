5e509b366a06eaa33ecd6c5e81b5e972697405b6
mafiapatch <g8071107@gmail.com>
2023-11-30 14:41:59 +0800

    [modify] update serval issue

--- src/views/FinalView.vue
+++ src/views/FinalView.vue
@@ -1,4 +1,23 @@
+<script>
+import { useRouter, useRoute } from "vue-router";
+
+export default {
+
+  setup() {
+    const router = useRouter();
+
+    const toNextPage = () => {
+      playBackgroundMusic()
+      router.push({ name: "home" });
+    };
+    return {
+      toNextPage
+    };
+  },
+};
+</script>
+
 <template>
-  <main class="final_bg">
+  <main class="final_bg" @click="toNextPage()">
   </main>
 </template>
